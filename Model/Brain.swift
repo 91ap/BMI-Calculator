591e00d5607cca7b467d1994335dfb773e0e6612
Andy Patterson <andyp.dev@proton.me>
2025-02-22 02:43:05 -0600

    [Task] MVC preparations complete. -> [Task] Assets added -> [Task] object placements complete

---
+++ Model/Brain.swift
@@ -0,0 +1,8 @@
+//
+//  Brain.swift
+//  BMI Calculator
+//
+//  Created by apat on 2/22/25.
+//
+
+import Foundation
---
+++ Model/CalculatorBrain.swift
@@ -0,0 +1,8 @@
+//
+//  CalculatorBrain.swift
+//  BMI Calculator
+//
+//  Created by apat on 2/22/25.
+//
+
+import Foundation
